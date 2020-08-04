7541057cb4dd25268a735bc90e663e9ba8b9b3dd
Daniil Baturin <daniil@baturin.org>
2020-08-04 21:55:44 +0700

    Add pretty-printing options.

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ function format(obj) {
     : JSON.stringify(obj);
 }
 
-module.exports = function(hash) {
-  function visit(hash, prefix) {
+module.exports = function(hash, indent=0, newline_after_section=false) {
+  function visit(hash, prefix, indent, newline_after_section) {
     var nestedPairs = [];
     var simplePairs = [];
 
@@ -23,26 +23,31 @@ module.exports = function(hash) {
 
     if (!(isEmpty(prefix) || isEmpty(simplePairs))) {
       toml += '[' + prefix + ']\n';
+      var indent_str = "".padStart(indent, " ");
     }
 
     forEach(simplePairs, function(array) {
       var key = array[0];
       var value = array[1];
 
-      toml += key + ' = ' + format(value) + '\n';
+      toml += indent_str + key + ' = ' + format(value) + '\n';
     });
+
+    if (!isEmpty(simplePairs) && newline_after_section) {
+      toml += '\n';
+    }
 
     forEach(nestedPairs, function(array) {
       var key = array[0];
       var value = array[1];
 
-      visit(value, isEmpty(prefix) ? key.toString() : [prefix, key].join('.'));
+      visit(value, isEmpty(prefix) ? key.toString() : [prefix, key].join('.'), indent, newline_after_section);
     });
   }
 
   var toml = '';
 
-  visit(hash, '');
+  visit(hash, '', indent, newline_after_section);
 
-  return toml;
+  return toml.trim();
 };
