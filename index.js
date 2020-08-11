c595fc96c3e60f1ebfda341d693bbac6805074f3
Daniil Baturin <daniil@baturin.org>
2020-08-11 11:24:44 +0700

    Use camelCase for the option names.

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ module.exports = function(hash, options={}) {
       toml += indent_str + key + ' = ' + format(value) + '\n';
     });
 
-    if (!isEmpty(simplePairs) && options.newline_after_section) {
+    if (!isEmpty(simplePairs) && options.newlineAfterSection) {
       toml += '\n';
     }
 
